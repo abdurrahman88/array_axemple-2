14f9c0136cfe1af69e8f385bb8976e46e6deb040
abdurrahman88 <amaralfaruq88@gmail.com>
2017-09-10 17:33:28 +0700

    array_dimensional.php

---
+++ array_dimensional.php
@@ -0,0 +1,17 @@
+<?php
+	$motor = array
+	(
+		array("Ninja",12,22),
+		array("Supra",13,23),
+		array("Kawasaki",14,24),
+		array("Jupiter MX",15,25)
+	);
+	echo $motor[0][0].": In stock: ".$motor[0][1].", sold: ".$motor[0][2].".<br>";
+	echo $motor[1][0].": In stock: ".$motor[1][1].", sold: ".$motor[1][2].".<br>";
+	echo $motor[2][0].": In stock: ".$motor[2][1].", sold: ".$motor[2][2].".<br>";
+	echo $motor[3][0].": In stock: ".$motor[3][1].", sold: ".$motor[3][2].".<br>"; 	
+	echo "<hr>"
+
+
+
+?>
